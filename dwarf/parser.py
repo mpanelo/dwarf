69dfe13b54f966bd0d304f20f462ab834c30c548
Mauricio Panelo <17281354+mpanelo@users.noreply.github.com>
2019-07-21 23:38:24 -0700

    Refactor Parser to be more D.R.Y.

--- dwarf/parser.py
+++ dwarf/parser.py
@@ -21,78 +21,78 @@ INSTRUCTION_FORMAT = {
 class Parser(object):
     def __init__(self, lexer):
         self.lexer = lexer
-        self.peek_token = self.lexer.next_token()
-        self.next_token()
 
     def parse_program(self):
         program = ast.Program()
 
-        while self.curr_token.type != token.EOF:
-            program.append(self.parse_statement())
-            self.next_token()
+        curr_token = self.lexer.next_token()
+        while curr_token.type != token.EOF:
+            program.append(self.parse_statement(curr_token))
+            curr_token = self.lexer.next_token()
 
         return program
 
-    def parse_statement(self):
-        if self.curr_token.type != token.INSTRUCTION:
-            raise ParseError("Statement must begin with an instruction")
-        if self.curr_token.literal not in INSTRUCTIONS:
-            raise ParserError("Invalid instruction {}".format(
-                self.curr_token.literal))
+    def parse_statement(self, instr_token):
+        self.assert_token(token.INSTRUCTION, instr_token)
 
-        format = INSTRUCTION_FORMAT.get(self.curr_token.literal)
+        format = INSTRUCTION_FORMAT.get(instr_token.literal)
 
         if format == R_FORMAT:
-            return self.parse_rformat_statement()
+            return self.parse_rformat_statement(instr_token)
         elif format == I_FORMAT:
-            return self.parse_iformat_statement()
+            return self.parse_iformat_statement(instr_token)
         else:
-            raise ParseError("No format exists for {}".format(self.curr_token))
+            raise ParseError("No format exists for {}".format(instr_token))
 
-    def parse_rformat_statement(self):
-        instr_token = self.curr_token
-        self.next_token()
+    def parse_rformat_statement(self, instr_token):
+        reg_token = self.lexer.next_token()
+        self.assert_token(token.REGISTER, reg_token)
+        return ast.RFormatStatement(instr_token, reg_token)
 
-        if self.curr_token.type != token.REGISTER:
+    def parse_iformat_statement(self, instr_token):
+        reg_token = self.lexer.next_token()
+        self.assert_token(token.REGISTER, reg_token)
+
+        comma_token = self.lexer.next_token()
+        self.assert_token_type(token.COMMA, comma_token.type)
+
+        binary_token = self.lexer.next_token()
+        self.assert_token(token.BINARY, binary_token)
+
+        return ast.IFormatStatement(instr_token, reg_token, binary_token)
+
+    def assert_token(self, expected_token_type, actual_token):
+        self.assert_token_type(expected_token_type, actual_token.type)
+        self.assert_token_literal(expected_token_type, actual_token.literal)
+
+    def assert_token_type(self, expectedType, actualType):
+        if expectedType != actualType:
+            raise ParseError("Expected {}, got {}".format(
+                expectedType, actualType))
+
+    def assert_token_literal(self, expected_token_type, token_literal):
+        if expected_token_type == token.INSTRUCTION:
+            self.assert_instruction(token_literal)
+        elif expected_token_type == token.REGISTER:
+            self.assert_register(token_literal)
+        elif expected_token_type == token.BINARY:
+            self.assert_binary(token_literal)
+
+    def assert_instruction(self, token_literal):
+        if token_literal not in INSTRUCTIONS:
             raise ParseError(
-                "{} must be followed by a register".format(instr_token))
-        if self.curr_token.literal not in REGISTERS:
-            raise ParseError("Invalid register {}".format(
-                self.curr_token.literal))
+                "Unknown instruction {}, must be one of {}".format(
+                    token_literal, INSTRUCTIONS))
 
-        return ast.RFormatStatement(instr_token, self.curr_token)
+    def assert_register(self, token_literal):
+        if token_literal not in REGISTERS:
+            raise ParseError("Unknown register {}, must be one of {}".format(
+                token_literal, REGISTERS))
 
-    def parse_iformat_statement(self):
-        instr_token = self.curr_token
-        self.next_token()
-
-        if self.curr_token.type != token.REGISTER:
-            raise ParseError(
-                "{} must be followed by a register".format(instr_token))
-        if self.curr_token.literal not in REGISTERS:
-            raise ParseError("Invalid register {}".format(
-                self.curr_token.literal))
-
-        reg_token = self.curr_token
-        self.next_token()
-
-        if self.curr_token.type != token.COMMA:
-            raise ParseError("Expected ',' after {}".format(reg_token))
-
-        self.next_token()
-
-        if self.curr_token.type != token.BINARY:
-            raise ParseError(
-                "{} must be followed by a binary".format(reg_token))
-        if len(self.curr_token.literal[2:]) != 4:
-            raise ParseError("Invalid binary {}".format(
-                self.curr_token.literal))
-
-        return ast.IFormatStatement(instr_token, reg_token, self.curr_token)
-
-    def next_token(self):
-        self.curr_token = self.peek_token
-        self.peek_token = self.lexer.next_token()
+    def assert_binary(self, token_literal):
+        if len(token_literal[2:]) != 4:
+            raise ParseError("Expected a 4-bit binary literal, got {}".format(
+                token_literal))
 
 
 class ParseError(Exception):
