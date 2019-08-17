12296eeb990890bbb6c495fe506246035807f8aa
Mauricio Panelo <17281354+mpanelo@users.noreply.github.com>
2019-08-17 12:28:46 -0700

    Use sets to determine if an instruction is format R or I

--- dwarf/parse/parser.py
+++ dwarf/parse/parser.py
@@ -1,9 +1,6 @@
 from dwarf.lex import token
 from dwarf.parse import ast
 
-R_FORMAT = 'R'
-I_FORMAT = 'I'
-
 INSTRUCTIONS = set([token.LUI, token.ORI, token.LB,
                     token.PUSH, token.SB, token.ADD])
 REGISTERS = set([
@@ -11,14 +8,17 @@ REGISTERS = set([
     token.REG6, token.REG7
 ])
 
-INSTRUCTION_FORMAT = {
-    token.LUI: I_FORMAT,
-    token.ORI: I_FORMAT,
-    token.LB: R_FORMAT,
-    token.PUSH: R_FORMAT,
-    token.ADD: R_FORMAT,
-    token.SB: R_FORMAT,
-}
+FORMAT_I = set([
+    token.LUI,
+    token.ORI,
+])
+
+FORMAT_R = set([
+    token.LB,
+    token.PUSH,
+    token.ADD,
+    token.SB,
+])
 
 
 class Parser(object):
@@ -30,29 +30,27 @@ class Parser(object):
 
         curr_token = self.lexer.next_token()
         while curr_token.type != token.EOF:
-            program.append(self.parse_statement(curr_token))
+            program.append(self.parse_instruction(curr_token))
             curr_token = self.lexer.next_token()
 
         return program
 
-    def parse_statement(self, instr_token):
+    def parse_instruction(self, instr_token):
         self.assert_token(token.INSTRUCTION, instr_token)
 
-        format = INSTRUCTION_FORMAT.get(instr_token.literal)
+        if instr_token.literal in FORMAT_R:
+            return self.parse_r_instruction(instr_token)
+        if instr_token.literal in FORMAT_I:
+            return self.parse_i_instruction(instr_token)
 
-        if format == R_FORMAT:
-            return self.parse_rformat_statement(instr_token)
-        elif format == I_FORMAT:
-            return self.parse_iformat_statement(instr_token)
-        else:
-            raise ParseError("No format exists for {}".format(instr_token))
+        raise ParseError("No format exists for {}".format(instr_token))
 
-    def parse_rformat_statement(self, instr_token):
+    def parse_r_instruction(self, instr_token):
         reg_token = self.lexer.next_token()
         self.assert_token(token.REGISTER, reg_token)
         return ast.RTypeInstruction(instr_token, reg_token)
 
-    def parse_iformat_statement(self, instr_token):
+    def parse_i_instruction(self, instr_token):
         binary_token = self.lexer.next_token()
         self.assert_token(token.BINARY, binary_token)
         return ast.ITypeInstruction(instr_token, binary_token)
